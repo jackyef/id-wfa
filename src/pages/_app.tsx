3a3a77890eda2b63f684e18d4825a71e88712bfa
jackyef <jacky.efendi1@gmail.com>
2022-04-21 17:59:07 +0700

    Add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,34 @@ import '../styles/tailwind.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import clsx from 'clsx';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {' '}
       <Head>
-        <meta name="robots" content="noindex" />
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src="https://www.googletagmanager.com/gtag/js?id=G-PL5F6WJMKJ"
+            ></Script>
+            <Head>
+              {/* Global site tag (gtag.js) - Google Analytics */}
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-PL5F6WJMKJ');`,
+                }}
+              ></script>
+            </Head>
+          </>
+        )}
       </Head>
       <Component {...pageProps} />
     </>
