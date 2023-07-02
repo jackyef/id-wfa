19615d7853f59d95343798506bfd9d9eed79573e
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-07-02 17:17:59 +0700

    feat: disclaimer page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,21 @@
+import ShieldExclamationIcon from '@heroicons/react/outline/ShieldExclamationIcon';
 import CodeIcon from '@heroicons/react/outline/CodeIcon';
 import AtSymbolIcon from '@heroicons/react/outline/AtSymbolIcon';
 import clsx from 'clsx';
+import Link from 'next/link';
 
 export const Footer = () => {
   return (
     <footer className={clsx('text-slate-700')}>
       <nav>
         <ul className={clsx('flex', 'space-x-4', 'justify-end')}>
+          <li>
+            <Link href="/disclaimer">
+              <a aria-label="See code on GitHub" title="See code on GitHub">
+                <ShieldExclamationIcon className={clsx('h-6', 'w-6')} />
+              </a>
+            </Link>
+          </li>
           <li>
             <a
               href="https://github.com/jackyef/id-wfa"
